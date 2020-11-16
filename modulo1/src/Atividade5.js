acd7691ade0f0dfaf12c9bfc0fcfe3ac2b57bd3a
tbcharlie <tbcharlie1@gmail.com>
2020-11-15 23:20:43 -0300

    302, atividade pronta

--- modulo1/src/Atividade5.js
+++ modulo1/src/Atividade5.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import Loader from "./atividade5/Loader";
+import Produto from "./atividade5/Produto";
 
-const Atividade5 = () =>{
-    return(
-        <div>
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
 
-        </div>
-    )
-}
+const Atividade5 = () => {
+  const [loader, setLoader] = React.useState(false);
+  const [dados, setDados] = React.useState(null);
+  const [preferPro, setPreferProd] = React.useState(null);
 
-export default Atividade5
+  React.useEffect(async () => {
+    if (localStorage.getItem("preferenciaProduto") !== null) {
+      const produtoPrefer = localStorage.getItem("preferenciaProduto");
+      setLoader(true);
+      const response = await fetch(
+        `https://ranekapi.origamid.dev/json/api/produto/${produtoPrefer}`
+      );
+      const json = await response.json();
+      setDados(json);
+      setLoader(false);
+      setPreferProd(produtoPrefer);
+    }
+  }, []);
+
+  const HanderClick = async (event) => {
+    setLoader(true);
+    const escolha = event.target.innerText;
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${escolha}`
+    );
+    const json = await response.json();
+    setPreferProd(escolha);
+    localStorage.setItem("preferenciaProduto", escolha);
+    setDados(json);
+    setLoader(false);
+  };
+
+  return (
+    <div>
+      <h1>Produto Preferido: {preferPro}</h1>
+      <button onClick={HanderClick} style={{ marginRight: "10px" }}>
+        notebook
+      </button>
+      <button onClick={HanderClick} style={{ marginRight: "10px" }}>
+        smartphone
+      </button>
+      {loader ? <Loader /> : null}
+      {!loader && dados && <Produto dados={dados} />}
+    </div>
+  );
+};
+
+export default Atividade5;
--- modulo1/src/index.js
+++ modulo1/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 // import Atividade1 from './Atividade1';
 // import Atividade2 from './Atividade2';
 // import Atividade3 from './Atividade3';
-import Atividade4 from './Atividade4';
-// import Atividade5 from './Atividade5';
+// import Atividade4 from './Atividade4';
+import Atividade5 from './Atividade5';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Atividade4 />
+    <Atividade5 />
   </React.StrictMode>,
   document.getElementById('root')
 );
